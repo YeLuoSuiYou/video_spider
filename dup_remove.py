a412fbe7ad9616276b9620e448607c11c480e396
ye <1264631168@qq.com>
2024-05-11 11:06:28 +0000

    add keyframe

---
+++ dup_remove.py
@@ -0,0 +1,78 @@
+import os
+import re
+from concurrent.futures import ThreadPoolExecutor
+
+import torch
+from PIL import Image
+from torchvision import transforms
+from tqdm import tqdm
+
+save_path = "/qiguojun/story/howto"
+
+transform = transforms.Compose(
+    [
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ]
+)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# model = torch.hub.load("facebookresearch/dino:main", "dino_vitb8")
+torch.hub.set_dir("/liujinxiu/ye/models/torch_hub")
+model = torch.hub.load("facebookresearch/dinov2", "dinov2_vitb14")
+model.eval().cuda()
+
+
+@torch.inference_mode()
+def image_dedup_reg(image_path):
+    pre_feature = torch.zeros(1, 768).to(device)
+    pre_i_path = ""
+
+    # sort according to the numerical order instead of alphabetic order
+    def fns(s):
+        return sum(((s, int(n)) for s, n in re.findall("(\\D+)(\\d+)", "a%s0" % s)), ())
+
+    l = len(sorted(os.listdir(image_path), key=fns))
+    i = 0
+
+    for image_name in sorted(os.listdir(image_path), key=fns):
+        i_path = image_path + "/" + image_name
+
+        if os.path.splitext(os.path.basename(i_path))[1] == ".jpg":
+            image = transform(Image.open(i_path)).unsqueeze(0).to(device)  # type: ignore
+
+            image_feature = model(image)
+            similarity = torch.cosine_similarity(image_feature, pre_feature, dim=1)
+
+            if similarity.item() >= 0.75:
+                # print(i_path + ":  " + str(similarity.item()))
+                if os.path.isfile(pre_i_path):
+                    os.remove(pre_i_path)
+            else:
+                pre_feature = image_feature
+            pre_i_path = i_path
+
+            i += 1
+
+    torch.cuda.empty_cache()
+
+
+def process_folder(video_no):
+    image_path = save_path + "/" + video_no
+    try:
+        with torch.no_grad():
+            image_dedup_reg(image_path)
+        # print("Finished Video: " + video_no)
+    except Exception:
+        print("Failed Video: " + video_no)
+
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        folders = sorted(os.listdir(save_path))
+        list(tqdm(executor.map(process_folder, folders), total=len(folders)))
+    # for video_no in tqdm(sorted(os.listdir(save_path))):
+    #     process_folder(video_no)
---
+++ extract_keyframes.py
@@ -0,0 +1,81 @@
+import concurrent.futures
+import json
+import os
+import pickle
+import subprocess
+
+import cv2
+from tqdm import tqdm
+
+import decord
+
+
+class VideoReaderWrapper(decord.VideoReader):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.seek(0)
+
+    def __getitem__(self, key):
+        frames = super().__getitem__(key)
+        self.seek(0)
+        return frames
+
+
+# Save the keyframes with type I
+def save_i_keyframes(video_fn, v_id, s_path):
+    decord_vr = VideoReaderWrapper(video_fn, num_threads=1)
+    i_frames = decord_vr.get_key_indices()
+    if len(i_frames) == 0:
+        print("No I-frames in " + video_fn)
+        del decord_vr, i_frames
+        return
+    keyframes = decord_vr.get_batch(i_frames).asnumpy()
+    milliseconds = decord_vr.get_frame_timestamp(i_frames)
+    milliseconds = [int(x[0] * 1000) for x in milliseconds]
+    for i, keyframe in enumerate(keyframes):
+        # Calculate the timestamp of the keyframe
+        millisecond = milliseconds[i]
+        seconds = millisecond // 1000
+        millisecond = millisecond % 1000
+        minutes = 0
+        hours = 0
+        if seconds >= 60:
+            minutes = seconds // 60
+            seconds = seconds % 60
+
+        if minutes >= 60:
+            hours = minutes // 60
+            minutes = minutes % 60
+        # timestamp of the keyframe
+        frame_time = str(int(hours)) + "-" + str(int(minutes)) + "-" + str(int(seconds)) + "-" + str(int(millisecond))
+        # Save the image
+        outname = str(v_id) + "_keyframe_" + str(frame_time) + ".jpg"
+        save_name = s_path + "/" + outname
+        cv2.imwrite(save_name, keyframe)
+    # release the video reader
+    del decord_vr, keyframes, i_frames, milliseconds
+
+
+def process_video(v_path, save_path, video_id):
+    save_name = video_id
+    s_path = save_path + "/" + save_name
+    if os.path.exists(s_path):
+        return
+    os.makedirs(s_path, exist_ok=True)
+    # Save the keyframes to the target dir
+    save_i_keyframes(v_path, save_name, s_path)
+
+
+if __name__ == "__main__":
+    video_dir = ""
+    save_path = ""
+    video_paths = []
+    for file in os.listdir(video_dir):
+        if file.endswith(".mp4"):
+            video_paths.append((os.path.join(video_dir, file), file.split(".")[0]))
+
+    for video_path in tqdm(video_paths, total=len(video_paths)):
+        try:
+            process_video(video_path[0], save_path, video_path[1])
+        except Exception as e:
+            print(e)
